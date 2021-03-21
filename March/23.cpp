c1108b9c3ce046ed69ecbf5c1216e9d81e57ad58
Sahil Silare <sahilsilare@gmail.com>
2021-03-21 14:57:53 +0530

    new file

---
+++ March/23.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    bool reorderedPowerOf2(int N)
+    {
+        string s1 = to_string(N);
+        sort(s1.begin(), s1.end());
+        for (int i = 0; i < 30; i++)
+        {
+            string s2 = to_string(1 << i);
+            sort(s2.begin(), s2.end());
+            if (s1 == s2)
+                return true;
+        }
+        return false;
+    }
+};
